db99632d9404073834b5fadf118d8ac78a8b709b
amangupta2509 <amangupta.t6@gmail.com>
2025-12-31 09:56:53 +0530

    Update admin routes

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -13,8 +13,13 @@ router.get('/equipment-utilization', adminController.getEquipmentUtilization);
 router.get('/user-productivity', adminController.getUserProductivity);
 router.get('/booking-analytics', adminController.getBookingAnalytics);
 
+router.get('/machine-analytics', adminController.getMachineUtilizationAnalytics);
+router.get('/daily-patterns', adminController.getDailyUsagePatterns);
+router.get('/peak-hours', adminController.getPeakHoursAnalysis);
+
 // User management
 router.get('/users', adminController.getAllUsers);
+router.get('/users/:id/details', adminController.getUserDetails);
 router.put('/users/:id/toggle-status', adminController.toggleUserStatus);
 
 module.exports = router;
